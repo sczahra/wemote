a00b55d3ae814c64b0b8218824f3b877b7c6f6bd
sczahra <sczahra@gmail.com>
2026-08-16 19:36:07 -0400

    Add WEMOTE service worker

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = "wemote-v0.4.2";
+const SHELL = ["/", "/styles.css", "/app.js", "/manifest.webmanifest"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)));
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
