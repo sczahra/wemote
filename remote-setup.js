621efb2be34ce4ba835843a8c9fd82b8e1400c9e
sczahra <sczahra@gmail.com>
2026-08-16 20:39:46 -0400

    WEMOTE v0.5.2: add remote bridge auto-link and token header

---
+++ remote-setup.js
@@ -0,0 +1,29 @@
+(() => {
+  const params = new URLSearchParams(location.search);
+  const bridge = params.get('bridge');
+  const token = params.get('token');
+  let changed = false;
+
+  if (bridge && /^https:\/\/[a-zA-Z0-9.-]+$/.test(bridge)) {
+    localStorage.setItem('wemoteBridgeUrl', bridge.replace(/\/$/, ''));
+    changed = true;
+  }
+  if (token && /^[a-fA-F0-9]{32,128}$/.test(token)) {
+    localStorage.setItem('wemoteRemoteToken', token);
+    changed = true;
+  }
+  if (changed) history.replaceState({}, document.title, location.pathname);
+
+  const nativeFetch = window.fetch.bind(window);
+  window.fetch = (input, init = {}) => {
+    const bridgeUrl = (localStorage.getItem('wemoteBridgeUrl') || '').replace(/\/$/, '');
+    const remoteToken = localStorage.getItem('wemoteRemoteToken') || '';
+    const url = typeof input === 'string' ? input : input.url;
+    if (bridgeUrl && remoteToken && url.startsWith(bridgeUrl + '/api/')) {
+      const headers = new Headers(init.headers || (typeof input !== 'string' ? input.headers : undefined) || {});
+      headers.set('X-Wemote-Token', remoteToken);
+      init = {...init, headers};
+    }
+    return nativeFetch(input, init);
+  };
+})();
