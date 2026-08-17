f1295d612e61ed3ba27b9a3e33100aac0d6c13f7
sczahra <sczahra@gmail.com>
2026-08-17 15:21:24 -0400

    WEMOTE v0.5.6 refresh service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wemote-v0.5.3";
+const CACHE = "wemote-v0.5.6";
 const SHELL = ["/", "/styles.css", "/remote-setup.js", "/app.js", "/manifest.webmanifest"];
 self.addEventListener("install", event => {
   self.skipWaiting();
